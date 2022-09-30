00ab086d4502b8e9b4af2818e1cc847a71b9440c
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2022-09-30 02:52:39 +0100

    rating component

---
+++ JavaScript/script.js
@@ -0,0 +1,28 @@
+// References
+let buttons = Array.from(document.getElementsByClassName("btn"));
+let Submit = document.querySelector(".submit");
+let Rate = document.getElementById("selectedN");
+let btnClicked = false;
+
+
+
+buttons.forEach(item =>{
+    item.addEventListener('click', () =>{
+        let activeBtn = document.querySelector(".active");
+        activeBtn?.classList.remove("active");
+        item.classList.add("active");
+        btnClicked = true;
+
+        Rate.innerHTML = item.innerText;
+    });
+});
+
+Submit.addEventListener("click", () => {
+    let beforeRating = document.querySelector(".beforeFeedback");
+    let afterRating = document.querySelector(".thankyou");
+
+    beforeRating.classList.add("down");
+    afterRating.classList.add("active");
+})
+
+
